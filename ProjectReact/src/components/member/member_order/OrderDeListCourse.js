2ac0093a458b537670f569d4357d2a09ea5275f9
BoAn Chen <troy0606troy0606@gmail.com>
2019-11-07 20:13:00 +0800

    fix order Detail list

--- ProjectReact/src/components/member/member_order/OrderDeListCourse.js
+++ ProjectReact/src/components/member/member_order/OrderDeListCourse.js
@@ -3,7 +3,15 @@ import React from "react";
 const OrderDeListCourse = props => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container-fluid">
+        <ul className="d-flex flex-column">
+          <li>Hello</li>
+          <li>Hello</li>
+          <li>Hello</li>
+          <li>Hello</li>
+          <li>Hello</li>
+        </ul>
+      </div>
     </>
   );
 };
