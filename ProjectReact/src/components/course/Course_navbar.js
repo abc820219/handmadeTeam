6984b6320070bcc67471f0a642d0e27893f11732
arden <qinwen1018@gmail.com>
2019-11-08 19:56:41 +0800

    更新

--- ProjectReact/src/components/course/Course_navbar.js
+++ ProjectReact/src/components/course/Course_navbar.js
@@ -13,7 +13,7 @@ class Course_navbar extends Component {
 
                     <div className="course_store_navbar">
                         <div id="course_store_info" className="course_store_info course_hover_color1">
-                            <img src="../image/course_img/info.png"   alt="" />
+                            <img src="/image/course_img/info.png"   alt="" />
                             <div className="course_store_info_wrap">
                             <div className="course_store_info_txt">INFO</div>
                             <div className="course_store_info_minTxt">introduction</div>
@@ -21,7 +21,7 @@ class Course_navbar extends Component {
                         </div>
 
                         <div id="course_store_web" className="course_store_info course_hover_color2">
-                        <img src="../image/course_img/web.png"   alt="" />
+                        <img src="/image/course_img/web.png"   alt="" />
                         <div className="course_store_info_wrap">
                             <div className="course_store_info_txt">WEB</div>
                             <div className="course_store_info_minTxt">website</div>
@@ -30,7 +30,7 @@ class Course_navbar extends Component {
                         </div>
 
                         <div id="course_store_map" className="course_store_info course_hover_color3">
-                        <img src="../image/course_img/map.png"   alt="" />
+                        <img src="/image/course_img/map.png"   alt="" />
                         <div className="course_store_info_wrap">
                             <div className="course_store_info_txt">MAP</div>
                             <div className="course_store_info_minTxt">map</div>
@@ -38,7 +38,7 @@ class Course_navbar extends Component {
                         </div>
 
                         <div id="course_store_classroom" className="course_store_info course_hover_color4">
-                            <img src="../image/course_img/classroom.png" alt="" />
+                            <img src="/image/course_img/classroom.png" alt="" />
                             <div className="course_store_info_wrap">
                             <div className="course_store_info_txt">SPACE</div>
                             <div className="course_store_info_minTxt">classroom</div>
@@ -56,24 +56,24 @@ class Course_navbar extends Component {
 
                     <main className="course_list_wrap">
                         <div id="course_list_a" className="course_list">
-                            <img src="../image/course_img/檸檬乳酪蛋糕s.png" alt="" />
-                            <img src="../image/course_img/烏龍鐵觀音乳酪蛋糕s.png" alt="" />
-                            <img src="../image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" />
+                            {/* <img src="/image/course_img/檸檬乳酪蛋糕s.png" alt="" />
+                            <img src="/image/course_img/烏龍鐵觀音乳酪蛋糕s.png" alt="" />
+                            <img src="/image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" /> */}
                         </div>
                         <div id="course_list_b" className="course_list">
-                            <img src="../image/course_img/鮮綠檸檬蛋糕s.png" />
-                            <img src="../image/course_img/檸檬乳酪蛋糕s.png" />
-                            <img src="../image/course_img/烏龍鐵觀音乳酪蛋糕s.png" />
+                            {/* <img src="/image/course_img/鮮綠檸檬蛋糕s.png" />
+                            <img src="/image/course_img/檸檬乳酪蛋糕s.png" />
+                            <img src="/image/course_img/烏龍鐵觀音乳酪蛋糕s.png" /> */}
                         </div>
                         <div id="course_list_c" className="course_list">
-                            <img src="../image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" />
-                            <img src="../image/course_img/鮮綠檸檬蛋糕s.png" alt="" />
-                            <img src="../image/course_img/檸檬乳酪蛋糕s.png" alt="" />
+                            {/* <img src="/image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" />
+                            <img src="/image/course_img/鮮綠檸檬蛋糕s.png" alt="" />
+                            <img src="/image/course_img/檸檬乳酪蛋糕s.png" alt="" /> */}
                         </div>
                         <div id="course_list_d" className="course_list">
-                            <img src="../image/course_img/烏龍鐵觀音乳酪蛋糕s.png" alt="" />
-                            <img src="../image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" />
-                            <img src="../image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" />
+                            {/* <img src="/image/course_img/烏龍鐵觀音乳酪蛋糕s.png" alt="" />
+                            <img src="/image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" />
+                            <img src="/image/course_img/玫瑰荔枝乳酪蛋糕s.png" alt="" /> */}
                         </div>
                     </main>
                 </section>
