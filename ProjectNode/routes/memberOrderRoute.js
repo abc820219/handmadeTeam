43b8d73cdf2dd83ccd8e1481b26981bf5bb4d5b0
BoAn Chen <troy0606troy0606@gmail.com>
2019-11-11 00:57:12 +0800

    add teachers REST to try

--- ProjectNode/routes/memberOrderRoute.js
+++ ProjectNode/routes/memberOrderRoute.js
@@ -1,23 +1,26 @@
-const express = require('express');//引入express應用程式
+const express = require("express"); //引入express應用程式
 const router = express.Router();
-const mysql = require('mysql');
-const db_Obj = require('C:/Users/__connect.json'); //連線到資料庫
+const mysql = require("mysql");
+const db_Obj = require("C:/Users/__connect.json"); //連線到資料庫
 const db = mysql.createConnection(db_Obj);
-const bluebird = require('bluebird');//使用兩次sql
+const bluebird = require("bluebird"); //使用兩次sql
 bluebird.promisifyAll(db);
+// import MemberOrder from "../domain/memberOrder";
 
-router.get('/', (req, res) => {
-    res.send("Member-OrderPage");
+router.get("/", (req, res) => {
+  res.send("Member-OrderPage");
 });
 
-router.get('/:id',(req, res) => {
-    const memberId = req.params.id;
-    sql = "SELECT `order`.*, `course_order`.* FROM `order` JOIN `course_order` ON `order`.order_sid = `course_order`.order_sid WHERE `order`.member_sid = " + memberId;
-    db.queryAsync(sql)
-    .then(results => {
-        console.log(results);
-        res.json(results[0]);c
-    })
+router.get("/:id", (req, res) => {
+  const memberId = req.params.id;
+  sql =
+    "SELECT `order`.*, `course_order`.* FROM `order` JOIN `course_order` ON `order`.order_sid = `course_order`.order_sid WHERE `order`.member_sid = " +
+    memberId;
+  db.queryAsync(sql).then(results => {
+    console.log(results);
+    res.json(results[0]);
+    c;
+  });
 });
 
 module.exports = router;
