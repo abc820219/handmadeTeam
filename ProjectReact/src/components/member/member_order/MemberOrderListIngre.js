39a339b717c17eee688befce5bd141f23c14aa22
Benson Chen <troy0606troy0606@gmail.com>
2019-11-11 10:51:39 +0800

    component MemberOrderList ---> Teacher/Course

--- ProjectReact/src/components/member/member_order/MemberOrderListIngre.js
+++ ProjectReact/src/components/member/member_order/MemberOrderListIngre.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { FaRegClock } from "react-icons/fa";
 import { FaDollarSign } from "react-icons/fa";
+import { GoPerson } from "react-icons/go";
 
-const MemberOrderListTeacher = () => {
+const MemberOrderListIngre = () => {
     return (
         <>
             <span>
-                <p>訂單編號: 100002</p>
+                <p>訂單編號: 100001</p>
             </span>
             <li className="d-flex justify-content-between align-item-center my-4">
                 <div>
-                    <h5>Sunday February 30th</h5>
                     <div>
                         <h5>
                             <FaRegClock className="oderList_icons" />
-                            老師名稱: 捷克史派羅
+                            商品名稱: 白巧克力
                   </h5>
                     </div>
                     <div className="d-flex flex-lg-wrap">
+                        <h5 className=" mr-3">
+                            <GoPerson className="oderList_icons" />
+                            數量: 5
+                  </h5>
                         <h5>
                             <FaDollarSign className="oderList_icons" />
                             價格: 2000
@@ -37,4 +41,4 @@ const MemberOrderListTeacher = () => {
     )
 }
 
-export default MemberOrderListTeacher
+export default MemberOrderListIngre
