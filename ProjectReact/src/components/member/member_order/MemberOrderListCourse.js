0423e808a2f8d4631ccf5a656d867ad72f357ee3
Benson Chen <troy0606troy0606@gmail.com>
2019-11-11 21:43:11 +0800

    ingredient/course list map orderList

--- ProjectReact/src/components/member/member_order/MemberOrderListCourse.js
+++ ProjectReact/src/components/member/member_order/MemberOrderListCourse.js
@@ -2,75 +2,25 @@ import React, { Component } from 'react';
 import { FaRegClock } from "react-icons/fa";
 import { FaDollarSign } from "react-icons/fa";
 
-const MemberOrderListCourse = () => {
+const MemberOrderListCourse = ({orderSid,courseName,courseOrderChoose,coursePrice}) => {
     return (
         <>
             <span>
-                <p>訂單編號: 100001</p>
+                <p>訂單編號: {orderSid}</p>
             </span>
             <li className="d-flex justify-content-between align-item-center my-4">
                 <div>
-                    <h5>Saturday February 20th</h5>
+                    <h5>{courseOrderChoose}</h5>
                     <div>
                         <h5>
                             <FaRegClock className="oderList_icons" />
-                            商品名稱: 巧克先生
+                            商品名稱: {courseName}
                   </h5>
                     </div>
                     <div className="d-flex flex-lg-wrap">
                         <h5>
                             <FaDollarSign className="oderList_icons" />
-                            價格: 2000
-                  </h5>
-                    </div>
-                </div>
-                <div>
-                    <figure>
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBVl9lhK94mJ5MxzGptFKO5FLRXoXVNDmjYndWy1H4kCaqDqOw"
-                            alt=""
-                        />
-                    </figure>
-                </div>
-            </li>
-            <li className="d-flex justify-content-between align-item-center my-4">
-                <div>
-                    <h5>Saturday February 20th</h5>
-                    <div>
-                        <h5>
-                            <FaRegClock className="oderList_icons" />
-                            商品名稱: 白巧克小姐
-                  </h5>
-                    </div>
-                    <div className="d-flex flex-lg-wrap">
-                        <h5>
-                            <FaDollarSign className="oderList_icons" />
-                            價格: 2000
-                  </h5>
-                    </div>
-                </div>
-                <div>
-                    <figure>
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBVl9lhK94mJ5MxzGptFKO5FLRXoXVNDmjYndWy1H4kCaqDqOw"
-                            alt=""
-                        />
-                    </figure>
-                </div>
-            </li>
-            <li className="d-flex justify-content-between align-item-center my-4">
-                <div>
-                    <h5>Saturday February 20th</h5>
-                    <div>
-                        <h5>
-                            <FaRegClock className="oderList_icons" />
-                            商品名稱: 白巧克小姐
-                  </h5>
-                    </div>
-                    <div className="d-flex flex-lg-wrap">
-                        <h5>
-                            <FaDollarSign className="oderList_icons" />
-                            價格: 2000
+                            價格: {coursePrice}
                   </h5>
                     </div>
                 </div>
