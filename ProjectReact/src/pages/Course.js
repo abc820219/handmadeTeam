687b02e50c255aa5ef45765207ad94a8c6dcec14
html.css practice <50762485+abc820219@users.noreply.github.com>
2019-11-13 21:46:25 +0800

    修正

--- ProjectReact/src/pages/Course.js
+++ ProjectReact/src/pages/Course.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-import Course_navbar from "../components/course/Course_navbar"
+import Course_navbar from "../components/course/Course_navbar";
+import Navbar from "../components/NavBar";
 
 class Course extends Component {
+  constructor(props) {
+    super();
+  }
   render() {
-    return <Course_navbar/>
+    return (
+      <>
+        <Navbar login={this.props.login} checkLogIn={this.props.checkLogIn} />
+        <Course_navbar />
+      </>
+    );
   }
 }
-
 
 export default Course;
