df67c7b7de766f1cb9a0d57f928c29d45e495b1c
Benson Chen <troy0606troy0606@gmail.com>
2019-11-26 10:27:09 +0800

    購物小車+叉叉功能

--- ProjectReact/src/components/cart/CartCourse.js
+++ ProjectReact/src/components/cart/CartCourse.js
@@ -1,31 +1,45 @@
-import React from "react";
-import { FaDollarSign } from "react-icons/fa";
+import React, { useContext } from "react";
 import { FaRegClock } from "react-icons/fa";
 import { GoPerson } from "react-icons/go";
 import { AiOutlineBook } from "react-icons/ai";
+import { MdCancel } from "react-icons/md";
+import { cancelCourse } from './CartAction';
+import CartStore from './CartStore'
 
-const CartCourse = ({ courseName, courseOrderApplicants, courseOrderChoose, courseOrderTime, courseLists }) => {
+const CartCourse = ({ courseName, courseOrderApplicants, courseOrderChoose, courseList, courseSid }) => {
+  
+  const { courseCart, id, cartCourseDispatch } = useContext(CartStore);
+  const courseSelect = (courseSid,courseOrderChoose) => {
+    let courseSelectCheck = courseCart.filter((courseC) => {
+      return courseC.course_sid == courseSid && courseC.course_order_choose == courseOrderChoose
+    })
+    cartCourseDispatch(cancelCourse(courseSelectCheck[0], id))
+  }
+
   return (
     <>
-      <ul className="d-flex justify-content-around align-items-center px-5">
+      <ul className="d-flex justify-content-around align-items-center px-5 course_sm_cart">
+        <div className='cancel_sm_cart'>
+          <MdCancel style={{ cursor: 'pointer' }} onClick={() => {courseSelect(courseSid,courseOrderChoose)}} />
+        </div>
         <li className="d-flex flex-column align-items-between py-3">
-          <div className="product-title">{courseOrderChoose}</div>
+          <div className="product-title">{courseOrderChoose ? courseOrderChoose.split(" ")[0] : ''}</div>
           <div className="d-flex align-items-center">
             <AiOutlineBook className="cartList_icons" />
             商品名稱:{courseName}
           </div>
           <div className="d-flex align-items-center">
             <FaRegClock className="cartList_icons" />
-            時間: {courseOrderTime}
+            時間: {courseOrderChoose ? courseOrderChoose.split(" ")[1] : ''}
           </div>
           <div className="d-flex align-items-center">
-            <FaDollarSign className="cartList_icons" />
+            <GoPerson className="cartList_icons" />
             {courseOrderApplicants}
           </div>
         </li>
         <figure>
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBVl9lhK94mJ5MxzGptFKO5FLRXoXVNDmjYndWy1H4kCaqDqOw"
+            src={"/image/course_img/360/" + courseList}
             alt=""
             width="80px"
             height="80px"
