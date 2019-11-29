ac7bd5e65a7ee35875ac8600d0e8be4b27a9f6cf
jason <z27089433@gmail.com>
2019-11-29 11:19:19 +0800

    ad

--- ProjectReact/src/pages/FindStore.js
+++ ProjectReact/src/pages/FindStore.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
-import { useAlert } from "react-alert";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+function Map() {
+  return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 12, lng: 12 }} />;
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
 function FindStore() {
-  const alert = useAlert();
-  useState(() => {
-    alert.show("Oh look, an alert!");
-  }, []);
-  return <> FindStore</>;
+  return (
+    <>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <MapWrapped
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCBFbHL-7A0VFRRARWIQCNJsE2HXq53z1g`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    </>
+  );
 }
 
 export default FindStore;
