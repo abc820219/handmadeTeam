0bc81b5441e4e7d27fc4f56733c5285cfd0cd7dc
jason <z27089433@gmail.com>
2019-12-03 20:14:19 +0800

    樣式修改

--- ProjectReact/src/components/NavBar.js
+++ ProjectReact/src/components/NavBar.js
@@ -80,6 +80,7 @@ const NavBar = ({ checkLogIn, login, ...props }) => {
               <FaTimesCircle
                 className="closeMenuButton"
                 onClick={() => setshowMenuBtn(false)}
+                style={{ cursor: "pointer" }}
               />
             </li>
             <li className="chooseLiHover">
